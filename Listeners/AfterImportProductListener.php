38ae9aad29d0cd9e061e01dc1be2ec1a220127aa
Your Name <you@example.com>
2020-08-12 12:24:27 -0300

    .

--- Listeners/AfterImportProductListener.php
+++ Listeners/AfterImportProductListener.php
@@ -5,6 +5,8 @@ namespace Modules\ProductStock\Listeners;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\ProductStock\Repositories\ProductStockRepository;
+use \PhpOffice\PhpSpreadsheet\Shared\Date;
+
 
 class AfterImportProductListener
 {
@@ -26,6 +28,12 @@ class AfterImportProductListener
      */
     public function handle($event)
     {
+        $data = $event->data();
+        try {
+                $data['date_delivery'] = Date::excelToDateTimeObject($data['date_delivery']);
+        } catch (Exception $e) {
+                // Do Nothing
+        }
         $product_stock = $event->product()->product_stock;
         ProductStockRepository::update($product_stock, $event->data());
     }
